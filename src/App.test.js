290548e716a28431e26dd08f507f966756bf7231
Adam <aalston9@gmail.com>
2020-07-11 02:34:20 -0400

    new test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Adam Alston/i);
   expect(linkElement).toBeInTheDocument();
 });
