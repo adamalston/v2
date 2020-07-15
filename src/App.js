43049091573d70373ec2dc841960a110da509eed
Adam <aalston9@gmail.com>
2020-07-15 00:53:35 -0400

    refactor App

--- src/App.js
+++ src/App.js
@@ -1,52 +1,14 @@
 import React from 'react';
 // import logo from './profile.png';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import registerIcons from './fontawesome.js';
-registerIcons();
+import Header from './Header'
+import Particle from './Particles';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				{/* <img src={logo} className="App-logo" alt="logo" /> */}
-				<h4>Adam Alston</h4>
-
-				<div>
-					<span className="bubble">
-						<a href="https://github.com/adamalston" target="_blank " rel="noopener noreferrer">
-							<FontAwesomeIcon className="icon" icon={['fab', 'github']} size="3x"/>
-							<span className="icon_title">Github</span>
-						</a>
-					</span>
-
-					<span className="bubble">
-						<a href="https://www.linkedin.com/in/adammalston/" target="_blank " rel="noopener noreferrer">
-							<FontAwesomeIcon className="icon" icon={['fab', 'linkedin']} size="3x"/>
-							<span className="icon_title">LinkedIn</span>
-						</a>
-					</span>
-
-					<span className="bubble">
-						<a href="resume.pdf" target="_blank " rel="noopener noreferrer">
-							<FontAwesomeIcon className="icon" icon={['fas', 'file-alt']} size="3x"/>
-							<span className="icon_title">Resume</span>
-						</a>
-					</span>
-
-					<span className="bubble">
-						<a href="mailto:aalston9@gmail.com" target="_blank " rel="noopener noreferrer">
-							<div className="temp">
-								<FontAwesomeIcon className="icon" icon={['fas', 'envelope']} size="3x"/>
-								<span className="icon_title">Email</span>
-							</div>
-						</a>
-					</span>
-
-				</div>
-
-			</header>
+			<Header/>
+			<Particle />
 		</div>
 	);
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders name', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Adam Alston/i);
-  expect(linkElement).toBeInTheDocument();
+	const { getByText } = render(<App />);
+	const linkElement = getByText(/Adam Alston/i);
+	expect(linkElement).toBeInTheDocument();
 });
