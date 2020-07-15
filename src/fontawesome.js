5f0ade3ad100ee3a6bdd747aaa692d094f8fd134
Adam <aalston9@gmail.com>
2020-07-15 01:07:47 -0400

    add paper-plane icon

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -2,13 +2,13 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 // import individual icons
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faFileAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFileAlt, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 export default function registerIcons() {
-    library.add(
-        faGithub,
-        faLinkedin,
-        faFileAlt,
-        faEnvelope
-    );
+	library.add(
+		faGithub,
+		faLinkedin,
+		faFileAlt,
+		faPaperPlane
+	);
 }
