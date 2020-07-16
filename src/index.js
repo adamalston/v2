6bcbfc951198c18e0c7b69daf59d39141259f540
Adam <aalston9@gmail.com>
2020-07-16 00:05:12 -0400

    refactor index

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
