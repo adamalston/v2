f65c1adb91de669afb97802e339b589f936aa11a
Adam <aalston9@gmail.com>
2020-07-16 00:06:41 -0400

    refactor App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-// import logo from './profile.png';
 import './App.css';
 import Header from './Header'
 import Particle from './Particles';
 
 function App() {
-	return (
-		<div className="App">
-			<Header/>
-			<Particle />
-		</div>
-	);
+    return (
+        <div className="App">
+            <Header/>
+            <Particle />
+        </div>
+    );
 }
 
 export default App;
