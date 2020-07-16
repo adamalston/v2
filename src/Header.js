8f55c7aea659f5767528443fd947044c8f76edb1
Adam <aalston9@gmail.com>
2020-07-16 00:09:34 -0400

    add headings and Button component

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Buttons from './Buttons';
+// import logo from './profile.png';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="App-header">
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+                <h1>Adam Alston</h1>
+
+                <h2>Software Engineer</h2>
+
+                {/* <p>I'm passionate about energy, space, and technology. If I'm not working on a project, you'll usually find me on a soccer field.</p>
+                <p>I have just completed my B.S. in Computer Science at the University of North Carolina - Chapel Hill.</p>
+                <p>If you're interested in what I'm working on now or what I've done in the past, be sure to check out my resume and the other links below.</p> */}
+                <Buttons />
+            </div>
+        )
+    }
+}
+
+export default Header;
