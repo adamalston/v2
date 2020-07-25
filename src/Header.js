53fdb0be69b6078e82f250ac9e188dc9bb33d489
Adam <aalston9@gmail.com>
2020-07-25 01:08:46 -0400

    aria-labels

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import Buttons from './Buttons.js';
+import Buttons from './Buttons';
 // import logo from './profile.png';
 
 class Header extends Component {
     render() {
         return (
-            <div className="App-header">
+            <div className="container">
                 {/* <img src={logo} className="App-logo" alt="logo" /> */}
-
-                <h1>Adam Alston</h1>
-
-                <h2>Software Engineer</h2>
+                <h1 className="name" aria-label="My name is Adam Alston">Adam Alston</h1>
+                <h2 className="title" aria-label="I am a software developer and engineer">Software Engineer</h2>
 
                 {/* <p>I'm passionate about energy, space, and technology. If I'm not working on a project, you'll usually find me on a soccer field.</p>
                 <p>I have just completed my B.S. in Computer Science at the University of North Carolina - Chapel Hill.</p>
