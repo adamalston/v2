cf27557bdeb7ed078057810a6beeed462ff7c306
Adam <aalston9@gmail.com>
2020-07-25 01:10:42 -0400

    add button testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App.js';
+import App from './App';
+import Buttons from './Buttons'
+import registerIcons from './FontAwesome';
 
 test('renders name', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/Adam Alston/i);
     expect(linkElement).toBeInTheDocument();
+});
+
+jest.mock('./FontAwesome.js', () => ({
+    __esModule: true,
+    default: jest.fn()
+}));
+
+test("registers icons before invocation", () => {
+    render(<Buttons />);
+    expect(registerIcons).toHaveBeenCalled();
 });
