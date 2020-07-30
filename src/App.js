2d1777769517b06acc270968b7afa088a2c13c52
Adam <aalston9@gmail.com>
2020-07-30 00:21:44 -0400

    add content component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js'
+import Content from './Content.js'
 import Particle from './Particles.js';
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+            <Content/>
             <Particle />
         </div>
     );
