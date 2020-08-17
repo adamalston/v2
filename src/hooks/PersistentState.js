7edd7443b0ced2842fa9299f612d5e5fb2efe38a
Adam <aalston9@gmail.com>
2020-08-17 00:50:39 -0400

    PersistentState

--- src/hooks/PersistentState.js
+++ src/hooks/PersistentState.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function usePersistantState(key, defaultValue) {
+export default function usePersistentState(key, defaultValue) {
     const [state, setState] = React.useState(() => {
-        const persistantState = localStorage.getItem(key);
-        return persistantState ? JSON.parse(persistantState) : defaultValue;
+        const persistentState = localStorage.getItem(key);
+        return persistentState ? JSON.parse(persistentState) : defaultValue;
     });
     React.useEffect(() => {
         window.localStorage.setItem(key, JSON.stringify(state));
