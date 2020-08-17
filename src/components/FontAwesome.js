bf98855f7f17a7534b94c3e65f923d389fcd7a45
Adam <aalston9@gmail.com>
2020-08-17 00:52:52 -0400

    new icons

--- src/components/FontAwesome.js
+++ src/components/FontAwesome.js
@@ -1,14 +1,18 @@
 // import the library
 import { library } from '@fortawesome/fontawesome-svg-core'
 // import individual icons
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faFileAlt, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons' //fab
+import { faFileAlt, faPaperPlane } from "@fortawesome/free-solid-svg-icons"; //fas
+import { faSun, faMoon } from "@fortawesome/free-regular-svg-icons"; //far
+
 
 export default function registerIcons() {
     library.add(
         faGithub,
         faLinkedin,
         faFileAlt,
-        faPaperPlane
+        faPaperPlane,
+        faSun,
+        faMoon,
     );
 }
