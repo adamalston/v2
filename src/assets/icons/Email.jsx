fc4a6e91ce3ca0c69362b367582ece3ac8c1002e
Adam <aalston9@gmail.com>
2020-09-06 21:28:06 -0400

    icons

---
+++ src/assets/icons/Email.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Email = () => (
+    <svg role="img" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+        <title>Email Icon</title>
+        <path fill="currentColor" d="M476.05 3.23L12.5 270.55a24 24 0 002.2 43.19l106.26 44.58L408.32 105.2c5.5-4.9 13.3 2.6 8.6 8.3L175.98 406.96v80.47c0 23.6 28.51 32.9 42.5 15.8L282 425.96l124.61 52.17a24.07 24.06 0 0033.01-18.19l72.02-431.86c3.42-20.25-18.31-34.85-35.59-24.85z"/>
+    </svg>
+);
+
+export default Email;
