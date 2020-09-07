d4a687a266e46211757a3c9a8ffd57ecfbf91299
Adam <aalston9@gmail.com>
2020-09-07 00:30:10 -0400

    components

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="name" aria-label="My name is Adam Alston">Adam Alston</h1>
+                <h2 className="title" aria-label="I am a software engineer">Software Engineer</h2>
+            </div>
+        )
+    }
+}
+
+export default Content;
