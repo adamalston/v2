58427db6c1961bdeb947f4c7d1015432a70dd195
Adam <aalston9@gmail.com>
2020-09-17 00:18:29 -0400

    base

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Toggle, Content, Buttons, Footer, Particles } from './components/'
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <div className='app'>
+            <Toggle />
+            <Content />
+            <Buttons />
+            <Footer />
+            <Particles />
+        </div>
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
