da409475bdd1a5b0156b3368deb01a60af75859c
Adam <aalston9@gmail.com>
2020-09-16 22:42:53 -0400

    icons

---
+++ src/assets/icons/Resume.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Resume = () => (
+    <svg role='img' viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg'>
+        <title>Resume Icon</title>
+        <path fill='currentColor' d='M288 136V0H88a23.94 23.94 0 00-24 24v464a23.94 23.94 0 0024 24h336a23.94 23.94 0 0024-24V160H312a24.07 24.07 0 01-24-24zm64 236a12 12 0 01-12 12H172a12 12 0 01-12-12v-8a12 12 0 0112-12h168a12 12 0 0112 12zm0-64a12 12 0 01-12 12H172a12 12 0 01-12-12v-8a12 12 0 0112-12h168a12 12 0 0112 12zm0-72v8a12 12 0 01-12 12H172a12 12 0 01-12-12v-8a12 12 0 0112-12h168a12 12 0 0112 12zm96-114.1v6.1H320V0h6.1a24 24 0 0117 7l97.9 98a23.92 23.92 0 017 16.9z' />
+    </svg>
+);
+
+export default Resume;
---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,6 @@
+export { default as Moon } from './Moon';
+export { default as Sun } from './Sun';
+export { default as GitHub } from './GitHub';
+export { default as LinkedIn } from './LinkedIn';
+export { default as Resume } from './Resume';
+export { default as Email } from './Email';
