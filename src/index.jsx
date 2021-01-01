61fad8461fb23f7d6df6b9965b20d10483681878
Adam <aalston9@gmail.com>
2020-12-31 21:30:04 -0500

    Who lights fireworks during the day? Remove light mode.

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import { Toggle, Content, Buttons, Footer, Particles } from './components/'
+import { Content, Buttons, Footer, Particles } from './components/'
 
 
 ReactDOM.render(
     <React.StrictMode>
         <div className='app'>
-            <Toggle />
             <Content />
             <Buttons />
             <Footer />
