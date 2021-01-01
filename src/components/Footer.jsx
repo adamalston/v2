bfcb7e69a451dd00679670314d1a793dd7c6c6f2
Adam <aalston9@gmail.com>
2020-12-31 21:19:43 -0500

    Happy New Year!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const isMobileDevice = window.matchMedia('(max-device-width: 820px) and (-webkit-min-device-pixel-ratio: 2)').matches;
 
-const Source = () => <>&nbsp;|&nbsp;<a className='footer-link' href='https://github.com/adamalston/v2'>Source</a></>
+const Source = () => <>&nbsp;|&nbsp;<a className='footer-link' href='https://github.com/adamalston/v2/tree/fireworks'>Source</a></>
 
 class Footer extends Component {
     render() {
