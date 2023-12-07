3a3cc789206278fbaa3e0c2702eb313a4fcf5093
Adam Alston <aalston9@gmail.com>
2023-12-06 20:21:36 -0500

    feat(commitlint): migrate from Angular to Conventional Commits (#75)

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-angular'],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Sets the maximum length for commit messages. It accounts for a 3-digit PR
+    // number (e.g., #123), and formatting (space and parentheses) in squash
+    // merges. Calculation: 72 (GitHub's limit) - 7 (3 digits for PR number +
+    // space + parentheses + hash symbol) = 65.
+    'header-max-length': [2, 'always', 65],
+  },
 };
